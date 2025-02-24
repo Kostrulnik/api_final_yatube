eecc8e68680bd3112f666ca89f485570fb67500e
Kostya Golovko <golowko.costya2010@gmail.com>
2025-02-24 08:39:01 +0300

    firts try

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# api_final
-api final
+# API для социальной сети Yatube
+
+## Описание
+Проект создан в рамках учебного курса Яндекс.Практикум.
+
+API сервис для проекта социальной сети Yatube.
+
+
+#### Доступный функционал
+- Python 3.9
+- Works on Linux, Windows, macOS
+- Django 3.2
+- Django Rest Framework
+- Pytest
+- Simple-JWT
+- SQLite3
+
+
+Запуск проекта в dev-режиме()
+----------
+
+1. Клонировать репозиторий и перейти в него в командной строке:
+```bash
+git clone git@github.com:Kostrulnik/api_yatube.git
+
+cd api_yatube
+```
+2. Cоздать и активировать виртуальное окружение:
+```bash
+python -m venv venv
+
+source venv/bin/activate
+```
+3. Установить зависимости из файла ```requirements.txt```:
+```bash
+python -m pip install --upgrade pip
+
+pip install -r requirements.txt
+```
+4. Выполнить миграции:
+```bash
+python manage.py migrate
+```
+5. Запустить проект:
+```bash
+python manage.py runserver
+```
+----------
+Примеры некоторых запросов API
+----------
+Получить список всех постов:  
+``` GET /api/v1/posts/ ```  
+Добавление нового поста:  
+``` POST /api/v1/posts/ ```   
+Получить список всех групп:  
+``` GET /api/v1/groups/ ```  
+Получить информацию о группе по id
+```GET /api/v1/groups/{group_id}/```
+Добавление нового комментария:  
+``` POST /api/v1/posts/{post_id}/comments/ ```  
+Удаление комментария по id:  
+``` DELETE /api/v1/posts/{post_id}/comments/{id}/ ```
+Получение списока подписок:  
+``` GET /api/v1/follow/ ```  
+Подписка пользователя на пользователя переданного в запросе:  
+``` POST /api/v1/follow/ ```  
+
+Документация к проекту
+---------
+Документация к API доступна по адресу [http://127.0.0.1:8000/redoc/](http://127.0.0.1:8000/redoc/) после запуска сервера с проектом
+
+Автор
+----------
+Головко Константин - [https://github.com/Kostrulnik](https://github.com/Kostrulnik)
--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
-
-from posts.models import Comment, Post
+from posts.models import Comment, Follow, Group, Post, User
 
 
 class PostSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Post."""
+    comments = serializers.StringRelatedField(
+        many=True,
+        read_only=True
+    )
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
@@ -14,6 +19,8 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 class CommentSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Comment."""
+    post = serializers.PrimaryKeyRelatedField(read_only=True)
     author = serializers.SlugRelatedField(
         read_only=True, slug_field='username'
     )
@@ -21,3 +28,48 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         fields = '__all__'
         model = Comment
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Group."""
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    """Сериализатор для модели Follow."""
+
+    user = serializers.SlugRelatedField(
+        slug_field='username',
+        read_only=True,
+        default=serializers.CurrentUserDefault()
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username',
+        queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('id', 'user', 'following')
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=('user', 'following'),
+                message='Вы уже подписывались на этого автора'
+            )
+        ]
+
+    def validate(self, data):
+        """Проверяем, что пользователь не подписывается на самого себя."""
+        request = self.context.get('request')
+        following = data.get('following')
+
+        if request and request.user == following:
+            raise serializers.ValidationError(
+                'Вы не можете подписаться на самого себя!'
+            )
+
+        return data
